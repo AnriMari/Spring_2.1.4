e9a10d98378b4541b141aefba14f368a55a3a365
Mariya Teterina <teterinamv@gmail.com>
2023-04-13 19:58:58 +0700

    Correction

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -2,6 +2,7 @@ package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,7 +19,7 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
-
+    @Bean
     public Timer getTimer() {
         return timer;
     }
